ca7f7083d7cb047e55916549c2a0bbea3cabc287
Provonsal <alecsw86@gmail.com>
2026-05-13 20:34:19 +0700

    fix: included api router

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI, Response
 
+
 from db.db import init_models
+from routes.api.api_router import router as api_router
 from routes.user.user_router import router as user_router
 from routes.admin.admin_router import router as admin_router
 
@@ -18,4 +20,5 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
 app.include_router(admin_router)
+app.include_router(api_router)
 
