10117081010815828edfd091e2338af3c836eeca
Provonsal <alecsw86@gmail.com>
2026-05-13 20:55:16 +0700

    fix: changed register api to correct method package (from get to post)

--- routes/api/get/__init__.py
+++ routes/api/get/__init__.py
@@ -1,8 +1,6 @@
-from .register import register_api_route
 from .refresh import refresh_api_route
 
 
 __all__ = [
-    "register_api_route",
     "refresh_api_route",
 ]
---
+++ routes/api/post/__init__.py
@@ -0,0 +1,6 @@
+from .register import register_api_route
+
+
+__all__ = [
+    "register_api_route"
+]
