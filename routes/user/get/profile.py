b7797b7f220474a7196d2149dc6d44a50c70ee0a
Provonsal <alecsw86@gmail.com>
2026-05-13 23:04:19 +0700

    feat: implemented profile route

--- routes/user/get/profile.py
+++ routes/user/get/profile.py
@@ -1,3 +1,7 @@
+from fastapi import Request
+
+from templates import templates
+
 from ...base_route import BaseRoute
 
 
@@ -7,7 +11,7 @@ class ProfileRoute(BaseRoute):
         self.path = 'profile'
         self.methods_type = "GET"
 
-    def endpoint(self):
-        ...
+    def endpoint(self, request: Request):
+        return templates.TemplateResponse(request, "after_logging_in_dummy.html")
 
 profile_route = ProfileRoute()    
