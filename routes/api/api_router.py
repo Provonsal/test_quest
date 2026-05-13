308647bdbd45d0776a69dcf59664e26572d6fd08
Provonsal <alecsw86@gmail.com>
2026-05-13 23:06:28 +0700

    fix: changed tag of api router (from 'users' to 'api')

--- routes/api/api_router.py
+++ routes/api/api_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 
 router = APIRouter(
     prefix="/api",  # Все пути будут начинаться с /users
-    tags=["users"],    # Тег для документации
+    tags=["api"],    # Тег для документации
     responses={404: {"description": "Not found"}}  # Общие ответы
 )
 
