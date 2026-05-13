c354d336423946c11aee032484705b275f6195a3
Provonsal <alecsw86@gmail.com>
2026-05-13 20:35:50 +0700

    fix: explicitly specified model names in import

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from models import Base
+from models import UserProfile, User, UserCredentials, RefreshToken, Role
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
