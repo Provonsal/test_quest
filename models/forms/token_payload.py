3f5c6e45b0c005a317386fd07329ed27e07d8365
Provonsal <alecsw86@gmail.com>
2026-05-18 13:52:26 +0700

    feat: added new forms for answers

---
+++ models/forms/token_payload.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[str] = None
+    exp: Optional[int] = None
+    type: Optional[str] = None
